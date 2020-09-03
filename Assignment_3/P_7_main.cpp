ea04e7f4f46b25257a724d9f5a69189e50b6d767
Prashantkumar1511 <68696462+Prashantkumar1511@users.noreply.github.com>
2020-09-03 23:24:49 +0530

    invoice driverprogram

---
+++ Assignment_3/P_7_main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include"P_7_invoice.h"
+using namespace std;
+
+int main()
+{
+    Invoice A;
+    int price,quantity;
+    string part_num,discription;
+  
+   
+        cout<<"Enter products Part number :  ";
+        cin>>part_num;
+        A.setPart_num(part_num);
+        cout<<"Enter Product's quantity :  ";
+        cin>>quantity;
+        A.setQuantity(quantity);
+        cout<<"Enter product's Price :  ";
+        cin>>price;
+        A.setPrice(price);
+        cout<<"Enter Discription :  ";
+        cin>>discription;
+        A.setDiscription(discription);
+        int Sum=A.calculate();
+        cout<<"Part number : "<<A.getPart_num()<<"  "<<"Quantity :  "<<A.getQuantity()<<"  "<<"Product's price :  "<<A.getPrice()<<"  "<<"Total price : "<<Sum<<"  "<<"DIscription :  "<<A.getDiscription();
+        return 0;
+
+        
+    
+}
